a6191e3f7b8fd81634f3984a916744031d5c1cf5
camgoucher123 <32824773+camgoucher123@users.noreply.github.com>
2017-11-17 23:52:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 var general = "373683685775376386"
 var cessnabotlog = "377912394946445313"
+var blue = "381291114516316170"
+var yellow = "381291160527831073"
+var red = "381291192626970624"
+var pink = "381299265097564161"
+var cyan = "381298867494453277"
+var black = "381303329881587715"
 client.on('ready',() => {
   console.log('Cessna Bot Online')
   client.user.setGame('Type -help for help!')
@@ -128,8 +134,13 @@ client.on('message', message => {
 
      },
      {
-       name: "???",
-       value: "??? (coming soon)"
+       name: "listcolors",
+       value: "Lists all the colors your can set your name"
+     },
+     {
+       name: "color",
+       value: "Changes the color of your name! -color [color]"
+
      }]
       }})
     } else
@@ -144,7 +155,83 @@ message.channel.send('Pinging!').then(m => m.edit(`Pong! Latency is ${m.createdT
   if(message.content.startsWith(prefix+'invite')){
     message.reply("The Discord invite code is: https://discord.gg/pcwKKEr")
   }
-
+  if(message.content.startsWith(prefix+'say')){
+    if(message.member.roles.find("name", "Bot Commander")){
+      if (args.length <=1) return
+      message.delete()
+    message.guild.channels.get(general).send(argsresult.slice(4))
+  }
+} else
+  if(message.content.startsWith(prefix+'color')){
+    let color = args[1];
+    if(!color) return message.reply("You must provide a color to choose from! Say \"-listcolors\" To view the colors")
+    if(args[1] === "blue" || args[1] === "Blue"){
+        message.member.removeRole(red,"ColorChange")
+          message.member.removeRole(black,"ColorChange")
+        message.member.removeRole(yellow,"ColorChange")
+        message.member.removeRole(pink,"ColorChange")
+        message.member.removeRole(cyan,"ColorChange")
+        message.reply("Your name color is now blue!")
+      message.member.addRole(blue,"ColorChange")
+    } else {
+      if(args[1] === "yellow" || args[1] === "Yellow"){
+          message.member.removeRole(red,"ColorChange")
+            message.member.removeRole(black,"ColorChange")
+          message.member.removeRole(blue,"ColorChange")
+          message.member.removeRole(pink,"ColorChange")
+            message.member.removeRole(cyan,"ColorChange")
+        message.reply("Your name color is now yellow!")
+        message.member.addRole(yellow,"ColorChange")
+      } else {
+        if(args[1] === "red" || args[1] === "Red"){
+            message.member.removeRole(blue,"ColorChange")
+            message.member.removeRole(yellow,"ColorChange")
+            message.member.removeRole(pink,"ColorChange")
+              message.member.removeRole(black,"ColorChange")
+              message.member.removeRole(cyan,"ColorChange")
+          message.reply("Your name color is now red!")
+          message.member.addRole(red,"ColorChange")
+        } else  {
+          if(args[1] === "pink" || args[1] === "Pink"){
+              message.member.removeRole(red,"ColorChange")
+              message.member.removeRole(blue,"ColorChange")
+              message.member.removeRole(yellow,"ColorChange")
+                message.member.removeRole(black,"ColorChange")
+                message.member.removeRole(cyan,"ColorChange")
+            message.reply("Your name color is now pink!")
+            message.member.addRole(pink,"ColorChange")
+          } else {
+            if(args[1] === "Cyan" || args[1] === "cyan"){
+                message.member.removeRole(red,"ColorChange")
+                message.member.removeRole(blue,"ColorChange")
+                message.member.removeRole(yellow,"ColorChange")
+                  message.member.removeRole(black,"ColorChange")
+                  message.member.removeRole(pink,"ColorChange")
+              message.reply("Your name color is now cyan!")
+              message.member.addRole(cyan,"ColorChange")
+          } else {
+            if(args[1] === "black" || args[1] === "Black"){
+                message.member.removeRole(red,"ColorChange")
+                  message.member.removeRole(cyan,"ColorChange")
+                message.member.removeRole(blue,"ColorChange")
+                message.member.removeRole(yellow,"ColorChange")
+                  message.member.removeRole(pink,"ColorChange")
+              message.reply("Your name color is now black!")
+              message.member.addRole(black,"ColorChange")
+          } else {
+            if(args[1] === "none" || args[1] === "none"){
+                message.member.removeRole(red,"ColorChange")
+                message.member.removeRole(blue,"ColorChange")
+                message.member.removeRole(yellow,"ColorChange")
+                  message.member.removeRole(pink,"ColorChange")
+                  message.member.removeRole(cyan,"ColorChange")
+              message.reply("Your name color has been removed")
+            } else{
+              message.reply("Invalid color, Say \"-listcolors\" To view the colors")
+          }}}}}}}} else
+            if(message.content.startsWith(prefix + 'listcolors')){
+          		message.reply("The colors are: ```pink, cyan, red, blue, yellow, black, none``` more colors coming soon!")
+          }
 });
 
 client.login(process.env.BOT_TOKEN);
